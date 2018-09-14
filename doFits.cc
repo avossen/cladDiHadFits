5229c126b956227f122915bb6d232cdc59bfe5e6
Anselm Vossen <agvossen@gmail.com>
2018-09-14 14:31:44 -0400

    prettyfying plots

--- doFits.cc
+++ doFits.cc
@@ -1,6 +1,8 @@
+#include "TPad.h"
 #include "TGraphErrors.h"
 #include "TGraph2DErrors.h"
 #include "TF2.h"
+#include "TF1.h"
 #include "TGaxis.h"
 #include "TStyle.h"
 #include "TLegend.h"
@@ -74,6 +76,7 @@ void doFit(float*** vals,float& amp1, float& amp1Err,float& amp2, float& amp2Err
 
 int main(int argc, char** argv)
 {
+  gStyle->SetOptFit();
   //lets assume that 16 is maxAngBins,
   int maxAngBins=16;
   int maxKinBins=50;
@@ -101,13 +104,14 @@ int main(int argc, char** argv)
   ifstream file;
   file.open(argv[1]);
   string line;
-  
+  int binIndex=0;  
   ///this tokenizes lines
   while(getline(file,line))
     {
       //and this space
       stringstream ssBinning(line);
       string binningName,sNumKinBins,sNumAngBins;
+
       string sKin;
       float kin=0;
       getline(ssBinning,binningName,' ');
@@ -120,11 +124,14 @@ int main(int argc, char** argv)
 	{
 	  ssBinning>>sKin;
 	  kin=stof(sKin);
+	  if(isnan(kin))
+	    kin=-1;
 	  meanKin[iKinBin]=kin;
 	  cout <<" mean " << binningName <<" bin " << iKinBin <<": " << kin << endl;
 	}
 
       cout <<"binningName: "<< binningName<<", " << numKinBins<<", " << numAngBins<<endl;
+      binIndex++;
       ///next line
       getline(file,line);
       stringstream ssVals(line);
@@ -133,13 +140,15 @@ int main(int argc, char** argv)
       float val;
       float rVal;
 
-      float y1[10];
-      float y2[10];
-      float x[10];
+      float y1[1000];
+      float y2[1000];
+      float x[1000];
 
-      float ey1[10];
-      float ey2[10];
-      float ex[10];
+      float ey1[1000];
+      float ey2[1000];
+      float ex[1000];
+      //in case some kin bins are empty
+      int graphIndex=0;
       for(int iKinBin=0;iKinBin<numKinBins;iKinBin++)
 	{
 	  ssVals>>srVal;
@@ -163,26 +172,82 @@ int main(int argc, char** argv)
 
 	  float amp2=0.0;
 	  float amp2Err=0.0;
-	  doFit(vals,amp1,amp1Err,amp2,amp2Err,numAngBins,rVal);
-	  y1[iKinBin]=amp1;
-	  ey1[iKinBin]=amp1Err;
-	  y2[iKinBin]=amp2;
-	  ey2[iKinBin]=amp2Err;
-	  x[iKinBin]=meanKin[iKinBin];
-	  ex[iKinBin]=0.0;
+	  if(meanKin[iKinBin]!=-1)
+	    {
+	      doFit(vals,amp1,amp1Err,amp2,amp2Err,numAngBins,rVal);
+	      y1[graphIndex ]=amp1;
+	      ey1[graphIndex]=amp1Err;
+	      y2[graphIndex]=amp2;
+	      ey2[graphIndex]=amp2Err;
+	      x[graphIndex]=meanKin[iKinBin];
+	      ex[graphIndex]=0.0;
+	      graphIndex++;
+
+	      cout <<"index : "<< graphIndex-1<<" y1: "<< y1[graphIndex-1];
+	      cout <<" y2: "<< y2[graphIndex-1] <<", ey1: "<< ey1[graphIndex-1];
+	      cout <<", ey2: "<< ey2[graphIndex-1] <<" x: " << x[graphIndex-1];
+	      cout<<" ex: "<< ex[graphIndex-1] <<endl;
+	    }
+	  else
+	    {
+	      cout <<"skipping kinbin: "<< iKinBin<<endl;
+	    }
+
 	  ///////
 
 	}
       TCanvas c;
-      TGraphErrors g1(numKinBins,x,y1,ex,ey1);
-      TGraphErrors g2(numKinBins,x,y2,ex,ey2);
+      cout <<"using " << graphIndex<< " points for graph " <<endl;
+      for(int i=0;i<graphIndex;i++)
+	{
+	  cout <<"x: "<< x[i] <<" y1: "<< y1[i] <<" y2: "<< y2[i] <<endl;
+	}
+      TGraphErrors g1(graphIndex,x,y1,ex,ey1);
+      TGraphErrors g2(graphIndex,x,y2,ex,ey2);
+      string xaxisLabel;
+      if(binIndex==1)
+	xaxisLabel="M [GeV]";
+      if(binIndex==2)
+	xaxisLabel="z";
+      if(binIndex==3)
+	xaxisLabel="x";
+      if(binIndex==4)
+	xaxisLabel="run number";
+
+	g1.SetMarkerStyle(24);
+	g2.SetMarkerStyle(24);
+	g1.GetYaxis()->SetLabelSize(0.04);
+	g1.GetYaxis()->SetTitleSize(0.06);
+	g1.GetYaxis()->SetTitle("A^{sin(#phi_{R})}");
+	g2.GetYaxis()->SetTitleSize(0.06);
+	g2.GetYaxis()->SetLabelSize(0.04);
+	g2.GetYaxis()->SetTitle("A^{sin(#phi_{H}-#phi_{R})}");
+
+
+
+	g1.GetXaxis()->SetLabelSize(0.04);
+	g1.GetXaxis()->SetTitle(xaxisLabel.c_str());
+	g2.GetXaxis()->SetLabelSize(0.04);
+	g2.GetXaxis()->SetTitle(xaxisLabel.c_str());
+	if(binIndex==4)
+	  {
+	    TF1 f1("f1","[0]",x[0],x[graphIndex]+0.1);
+	    f1.SetParameter(0,0.0);
+	    g1.Fit(&f1);
+	    f1.SetParameter(0,0.0);
+	    g2.Fit(&f1);
+	  }
       c.Divide(2,1);
-      c.cd(1);
-      g1.Draw("ALP");
-      c.cd(2);
-      g2.Draw("ALP");
+      TVirtualPad* pad1=c.cd(1);
+      pad1->SetLeftMargin(0.2);
+      g1.Draw("AP");
+      pad1=c.cd(2);
+      pad1->SetLeftMargin(0.3);
+      g2.Draw("AP");
       char buffer[300];
       sprintf(buffer,"asym2DFit_out_%s.png",binningName.c_str());
+      c.SaveAs(buffer);
+      sprintf(buffer,"asym2DFit_out_%s.root",binningName.c_str());
       c.SaveAs(buffer);
 
     }
